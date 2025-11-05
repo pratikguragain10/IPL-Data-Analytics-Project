f19de1e8754862f8e0b3874484b1d2953a990f10
pratikguragain10 <pratikguragain69@gmail.com>
2025-11-05 14:57:25 +0530

    Final Updates including images, CSVs, and deletions

--- slicing-logic/slicing-deliveries-logic.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-file = pd.read_csv('data/deliveries.csv')
-for i in range(2008,2018):
-    season_i=file[file['season'] == i]
-    season_i.to_csv(f'sliced_matches/match{i}.csv', index = False)
